b4cf494d259b928b8758bcdfbc7a1d6772196787
qxz23pf <ciamac.davoudi@partner.bmw.de>
2024-04-02 13:42:13 +0200

    app.vue initial commit

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- Header -->
+    <div>
+    <!-- Sidebar -->
+
+    <!-- Main Section -->
+    <main>
+
+      <!-- Status Cards -->
+
+      <!-- Customer List -->
+
+      <!-- Sales Chart -->
+
+      <!-- Recent Orders -->
+    </main>
+
+    <div>
+      <!-- Profile Info -->
+
+      <!-- Reminders -->
+    </div>
+    </div>
   </div>
 </template>
