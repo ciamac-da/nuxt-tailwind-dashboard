8b4eee163e4e2b2cc0ab380555b8dc53f7e208fc
qxz23pf <ciamac.davoudi@partner.bmw.de>
2024-04-02 13:30:09 +0200

    logo adjustment & page title & font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
   },
+  app: {
+    head: {
+      title: "Nuxtjs-Tailwind-Dashboard",
+      link: [
+        { rel: "icon", type:"image/x-icon", href: "/icon.svg" },
+
+        // inter font
+        { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
+        { rel: "preconnect", href: "https://rsms.me/" },
+      ]
+    }
+  }
 })
