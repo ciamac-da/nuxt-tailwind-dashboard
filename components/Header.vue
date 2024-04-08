3fd7dd6965aa4046a485881a309e111984c5cdd4
qxz23pf <ciamac.davoudi@partner.bmw.de>
2024-04-08 10:15:05 +0200

    dark/light mode

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="sticky top-0 z-20 border-b border-gray-800 bg-gray-400 backdrop-blur">
+    <header class="sticky top-0 z-20 border-b border-gray-800 bg-blue-400 backdrop-blur">
         <!-- Flex Container -->
         <div class="container flex h-16 items-center justify-between">
             <!-- Logo & Page Title-->
@@ -9,11 +9,24 @@
                 <!-- Page Title -->
                 <nuxt-link class="text-xl font-bold" to="/">Analytics Dashboard App</nuxt-link>
             </div>
+            <!-- Right side of header -->
+        <div class="flex items-center gap-5">
+            <button @click="toggleTheme"
+            class="flex h-9 shrink-0 items-center justify-center rounded-full border bg-background">
+            <Icon name="heroicons:sun" class="h-5 w-5" />
+            </button>
+        </div>
         </div>
     </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+const mode = useColorMode();
+  const toggleTheme = () => {
+    mode.value = mode.value === "dark" ? "light" : "dark";
+  };
+</script>
 
 <style>
 </style>
